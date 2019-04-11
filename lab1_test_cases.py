5a89216e3f18fe14412ae7a03a4645fbb1e915be
Lisa Li <lli27@unix5.csc.calpoly.edu>
2019-04-10 19:01:42 -0700

    updated test cases

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -7,12 +7,12 @@ class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
         """tests max_list_iter for lists with unique values, duplicates,
-           empty lists, and for max values in the last, first, middle, and 
+           empty lists, and for max values that are negative, and in the last, first, middle, and 
            when a list is None"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
-        self.assertEqual(max_list_iter([]), None)
+        self.assertIsNone(max_list_iter([]))
         self.assertEqual(max_list_iter([1, 2, 3]), 3)
         self.assertEqual(max_list_iter([1, 3, 2]), 3)
         self.assertEqual(max_list_iter([3, 1, 2]), 3)
@@ -20,7 +20,8 @@ class TestLab1(unittest.TestCase):
         self.assertEqual(max_list_iter([3, 3, 1]), 3)
         self.assertEqual(max_list_iter([1, 3, 3]), 3)
         self.assertEqual(max_list_iter([3, 1, 3]), 3)
-        
+        self.assertEqual(max_list_iter([0, 0, 0]), 0)
+        self.assertEqual(max_list_iter([-1, -2, -3]), -1)
 
     def test_reverse_rec(self):
         """tests reverse_rec for exception, for random list lengths, if the list
@@ -33,6 +34,8 @@ class TestLab1(unittest.TestCase):
         self.assertEqual(reverse_rec([0, 9, 8, 7, 6]), [6, 7, 8, 9, 0])
         self.assertEqual(reverse_rec([]), [])
         self.assertEqual(reverse_rec([1]), [1])
+        self.assertEqual(reverse_rec([5, 10]), [10, 5])
+        self.assertEqual(reverse_rec([3, 3, 3]), [3, 3, 3])
 
 
     def test_bin_search(self):
@@ -44,9 +47,10 @@ class TestLab1(unittest.TestCase):
         self.assertEqual(bin_search(4, 0, len(list_val) - 1, list_val), 4)
         self.assertEqual(bin_search(10, 0, 10, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 10)
         self.assertEqual(bin_search(0, 0, 2, [0, 1, 2]), 0)
-        self.assertEqual(bin_search(1, 0, 2, [0, 0, 0]), None)
+        self.assertIsNone(bin_search(1, 0, 2, [0, 0, 0]))
         self.assertEqual(bin_search(1, 0, 2, [0, 1, 2]), 1)
         self.assertEqual(bin_search(2, 0, 2, [0, 1, 2]), 2)
+        self.assertEqual(bin_search(-2, 0, 2, [-2, 0, 2]), 0)
         tlist = None
         with self.assertRaises(ValueError):
             bin_search(2, 3, 1, tlist)
@@ -54,5 +58,3 @@ class TestLab1(unittest.TestCase):
 
 if __name__ == "__main__":
         unittest.main()
-
-    
